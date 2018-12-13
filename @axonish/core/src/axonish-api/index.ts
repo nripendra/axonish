4834edfb2658106bf9cca826275065ff7a3c78e0
nripendra <nripendra.newa@gmail.com>
2018-12-13 18:36:50 +0545

    Ability to debug tests.

--- @axonish/core/src/axonish-api/index.ts
+++ @axonish/core/src/axonish-api/index.ts
@@ -5,5 +5,11 @@ type ClassOf<T> = {
 };
 type AxonishApiReturnType = (constructor: ClassOf<IApiStartup>) => void;
 export function AxonishApi(): AxonishApiReturnType {
-  return (constructor: ClassOf<IApiStartup>) => {};
+  return (constructor: ClassOf<IApiStartup>) => {
+    const ApiStartupClass = constructor;
+    const instance = new ApiStartupClass();
+    instance.config({} as any);
+    instance.starting({} as any);
+    instance.started({} as any, { address: "localhost", port: 3000 });
+  };
 }
