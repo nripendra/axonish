cf55f7698f2a92dfb7d22ba2a614edd75c036f3b
nripendra <nripendra.newa@gmail.com>
2018-12-13 18:44:33 +0545

    async/await support

--- @axonish/core/src/axonish-api/axonish-api.spec.ts
+++ @axonish/core/src/axonish-api/axonish-api.spec.ts
@@ -1,5 +1,5 @@
-import { TestFixture, Test, Expect } from "alsatian";
-import { AxonishApi } from ".";
+import { TestFixture, Test, Expect, AsyncTest } from "alsatian";
+import { AxonishApi, __AxonishApiAwaitForUnitTest } from ".";
 import IApiStartup, { ServerStartedInfo } from "../interfaces/IApiStartup";
 import IApiConfiguration from "../interfaces/IApiConfiguration";
 import { ApolloServer } from "apollo-server-express";
@@ -24,6 +24,31 @@ export class AxonishApiSpecs {
       }
       onError(err: any): void {}
     }
+    Expect(calledFunctions[0]).toBe("config");
+    Expect(calledFunctions[1]).toBe("starting");
+    Expect(calledFunctions[2]).toBe("started");
+  }
+
+  @AsyncTest(`Support promises in lifecycle functions`)
+  async supportPromise() {
+    const calledFunctions: string[] = [];
+    @AxonishApi()
+    class TestApi implements IApiStartup {
+      async config(appConfig: IApiConfiguration): Promise<any> {
+        await Promise.resolve();
+        calledFunctions.push("config");
+      }
+      async starting(graphqlServer: ApolloServer): Promise<void> {
+        await Promise.resolve();
+        calledFunctions.push("starting");
+      }
+      async started(graphqlServer: ApolloServer, info: ServerStartedInfo) {
+        await Promise.resolve();
+        calledFunctions.push("started");
+      }
+      onError(err: any): void {}
+    }
+    await __AxonishApiAwaitForUnitTest();
     Expect(calledFunctions[0]).toBe("config");
     Expect(calledFunctions[1]).toBe("starting");
     Expect(calledFunctions[2]).toBe("started");
--- @axonish/core/src/axonish-api/index.ts
+++ @axonish/core/src/axonish-api/index.ts
@@ -4,12 +4,35 @@ type ClassOf<T> = {
   new (...args: any[]): T;
 };
 type AxonishApiReturnType = (constructor: ClassOf<IApiStartup>) => void;
+
+/**
+ * HACK!! Just for awaiting in Unit tests.
+ */
+let _initilizingApiPromise: Promise<void> = Promise.resolve();
+export async function __AxonishApiAwaitForUnitTest() {
+  await _initilizingApiPromise;
+}
+
 export function AxonishApi(): AxonishApiReturnType {
   return (constructor: ClassOf<IApiStartup>) => {
     const ApiStartupClass = constructor;
     const instance = new ApiStartupClass();
-    instance.config({} as any);
-    instance.starting({} as any);
-    instance.started({} as any, { address: "localhost", port: 3000 });
+    _initilizingApiPromise = (async () => {
+      const configResult = instance.config({} as any);
+      if (configResult && configResult.then) {
+        await configResult;
+      }
+      const startingResult = instance.starting({} as any);
+      if (startingResult && startingResult.then) {
+        await startingResult;
+      }
+      const startedResult = instance.started({} as any, {
+        address: "localhost",
+        port: 3000
+      });
+      if (startedResult && startedResult.then) {
+        await startedResult;
+      }
+    })();
   };
 }
